75731db657cb4ed8b6e94fb8014947664651a2e1
Unknown <kevdes98@gmail.com>
2017-10-14 04:34:54 -0400

    api

--- pl_ai/api/urls.py
+++ pl_ai/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from views import CreateView
+from . import views
 
 urlpatterns = {
-        url(r'^notes/$', CreateView.as_view(), name="create"),
+        url(r'^notes/$', views.note_list, name="note_list"),
 }
 
-
+urlpatterns = format_suffix_patterns(urlpatterns)
