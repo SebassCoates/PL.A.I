0bb680bd2543e34e5d465d098468de630c1c2a3f
sebasscoates <sebastian.coates@tufts.edu>
2017-12-20 21:33:42 -0500

    Explitly prevent caching

--- app.py
+++ app.py
@@ -30,6 +30,19 @@ CORS(app)
 app.config['UPLOAD_FOLDER'] = DIR_ROOT
 socketio = SocketIO(app, engineio_logger=False, ping_timeout=30)
 
+@app.after_request
+def add_header(r):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        """
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
+
+
 @app.route('/', methods=["GET"])
 def index():
     return render_template('plai.html')
