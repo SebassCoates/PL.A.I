735059289a0dec0b51221cdc67573c21a2e64a31
sebasscoates <sebastian.coates@tufts.edu>
2017-12-26 22:46:14 -0800

    syntax

--- www/main.js
+++ www/main.js
@@ -44,7 +44,7 @@ function onMIDIFailure(error) {
 }
 
 function playImprov() {
-    MIDI.Player.loadFile('/improv.mid', MIDI.Player.start
+    MIDI.Player.loadFile('/improv.mid', MIDI.Player.start)
 
     MIDIjs.play('/improv.mid');
 }
